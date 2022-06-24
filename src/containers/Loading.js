7a87c65884b14d8cb21c9420e26f9dc190799625
Emeka Augustine <cafafanscoders@gmail.com>
2022-06-25 01:02:19 +0800

    Add loader

---
+++ src/containers/Loading.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { keyframes } from '@mui/system';
+import Stack from '@mui/material/Stack';
+import LinearProgress from '@mui/material/LinearProgress';
+import {ErrorBoundary} from '../errors/ErrorBandary'
+
+const HeartBeat = keyframes`
+    0% {opacity: 0.25; transform: scale(1);}
+    25% {opacity: 0.45; transform: scale(1.3);}
+    50% {opacity: 0.65; transform: scale(1.4);}
+    75% {opacity: 0.85; transform: scale(1.3);}
+    100% {opacity: 1.0; transform: scale(1);}
+`;
+
+
+const Div = styled("div")(({theme}) => ({
+    display: 'flex',
+    height: '100vh',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    background: `linear-gradient(${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+  }));
+  
+
+  const Image = styled("img")(() => ({
+    animation: `${HeartBeat} 1.5s linear infinite;`,
+    borderRadius: '10px',
+  }));
+
+
+
+function Loading() {
+    return ( 
+      <ErrorBoundary>
+        <Stack sx={{ width: '100%', color: 'grey.500'}}>
+            <LinearProgress color="success" />
+        </Stack>
+        <Div>
+            <Image src='images/logo.png' alt='Title' loading="lazy" width={50}/>
+        </Div>
+      </ErrorBoundary>
+     );
+}
+
+export default Loading;
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from '../containers/Loading';
 const Home  = React.lazy(() => import("../containers/Home"));
 const Games = React.lazy(() => import("../containers/Games"));
 const NotFound = React.lazy(() => import("../containers/NotFound"));
@@ -7,13 +8,14 @@ const NotFound = React.lazy(() => import("../containers/NotFound"));
 
 export default function AppRouter() {
     return (
-        <React.Suspense fallback={<p>Loading...</p>}>
+        <React.Suspense fallback={<Loading />}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="login" element={<Home />} />
                     <Route path="register" element={<Home />} />
                     <Route path="games" element={<Games />} />
+                    <Route path="loading" element={<Loading />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
