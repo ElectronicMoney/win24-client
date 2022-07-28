684fba1e6e5ac3d3fd59b31b218a0d6cd4947766
Emeka Augustine <cafafanscoders@gmail.com>
2022-07-28 16:09:25 +0800

    add refresh oken in the background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import AppRouter from './router'
+import { setAuthUser } from './features/auth/authSlice';
+import { useRefreshTokenApiMutation } from './services/authApi';
+
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const [refreshToken, { isLoading, isSuccess, data }] = useRefreshTokenApiMutation()
+
+  const auth = useSelector(state => state.auth)
+
+
+  React.useEffect(() => {
+
+    setInterval(() => {
+      // Run in the background every 1o minutes or 36000000 mili seconds
+      if(auth.isAuthenticated){
+
+        refreshToken()
+      }
+    }, 36000000)
+  })
+
+  if (isLoading) {
+      console.log("Creating Refresh token in the background...") 
+  }
+
+  if(isSuccess) {
+    dispatch(setAuthUser({accessToken: data.token}))
+  }
+
   return ( 
     <React.Fragment>
       <AppRouter />
