7239230a4e6df00c422533d78340229ff5433445
Emeka Augustine <cafafanscoders@gmail.com>
2022-08-04 11:53:32 +0800

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import AppRouter from './router'
-import { setAuthUser } from './features/auth/authSlice';
-import { useRefreshTokenApiMutation } from './services/authApi';
+// import { setAuthUser } from './features/auth/authSlice';
+// import { useRefreshTokenApiMutation } from './services/authApi';
 
 
 function App() {
 
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  const [refreshToken, { isLoading, isSuccess, data }] = useRefreshTokenApiMutation()
+  // const [refreshToken, { isLoading, isSuccess, data }] = useRefreshTokenApiMutation()
 
-  const auth = useSelector(state => state.auth)
+  // const auth = useSelector(state => state.auth)
 
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
 
-    setInterval(() => {
-      // Run in the background every 1o minutes or 36000000 
-      if(auth.isAuthenticated){
+  //   setInterval(() => {
+  //     // Run in the background every 1o minutes or 36000000 
+  //     if(auth.isAuthenticated){
 
-        refreshToken()
-      }
-    }, 36000000)
-  })
+  //       refreshToken()
+  //     }
+  //   }, 36000000)
+  // })
 
-  if (isLoading) {
-      console.log("Creating Refresh token in the background...") 
-  }
+  // if (isLoading) {
+  //     console.log("Creating Refresh token in the background...") 
+  // }
 
-  if(isSuccess) {
-    dispatch(setAuthUser({accessToken: data.token}))
-  }
+  // if(isSuccess) {
+  //   dispatch(setAuthUser({accessToken: data.token}))
+  // }
 
   return ( 
     <React.Fragment>
